ea08da7aaa03143ab7a45b0fdf7e92b93f40050f
sachin krupal uke <sachinkrupaluke@sachins-MacBook-Air.local>
2024-06-05 00:37:10 +0530

    js-cookie

--- Employer/vite.config.js
+++ Employer/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['axios','react-router-dom'],
+      external: ['axios','react-router-dom','js-cookie'],
     },
   },
 })
